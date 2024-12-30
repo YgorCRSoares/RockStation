ba6a44cc6e80e33d32920f04ac54d95125b65241
Ygor Soares <klavierkrieg@gmail.com>
2024-12-30 13:43:27 -0300

    introduce biome on the project

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,67 +1,66 @@
 // ReloadIFrame
 function reloadIframe(element) {
-    // Find the parent element of the clicked link
-    var parentDiv = element.parentElement;
+	// Find the parent element of the clicked link
+	const parentDiv = element.parentElement;
 
-    // Find the iframe within that parent element
-    var iframe = parentDiv.querySelector('iframe');
+	// Find the iframe within that parent element
+	const iframe = parentDiv.querySelector("iframe");
 
-    // Reload the iframe by modifying its src attribute
-    if (iframe) {
-        iframe.src = iframe.src;
-    }
+	// Reload the iframe by modifying its src attribute
+	if (iframe) {
+		iframe.src = iframe;
+	}
 }
 
 function hideUnhide(button) {
-    const rockSections = document.getElementsByClassName('RockSection');
-    const carouselBtn = document.getElementsByClassName('carousel-button');
-    for (var i = 0; i < rockSections.length; i++) {
-        if (rockSections[i].classList.contains('hidden')) {
-            rockSections[i].classList.remove('hidden');
-        } else {
-            rockSections[i].classList.add('hidden');
-        }
-    }
+	const rockSections = document.getElementsByClassName("RockSection");
+	const carouselBtn = document.getElementsByClassName("carousel-button");
+	for (let i = 0; i < rockSections.length; i++) {
+		if (rockSections[i].classList.contains("hidden")) {
+			rockSections[i].classList.remove("hidden");
+		} else {
+			rockSections[i].classList.add("hidden");
+		}
+	}
 }
 
 // Carousel
 let currentSlide = 0;
 
 function updateCarousel() {
-    const carouselWrapper = document.querySelector('.carousel-wrapper');
-    const totalSlides = document.querySelectorAll('.carousel-item').length;
-    const slideWidth = document.querySelector('.carousel-item').offsetWidth;
+	const carouselWrapper = document.querySelector(".carousel-wrapper");
+	const totalSlides = document.querySelectorAll(".carousel-item").length;
+	const slideWidth = document.querySelector(".carousel-item").offsetWidth;
 
-    carouselWrapper.style.transform = `translateX(-${currentSlide * slideWidth}px)`;
+	carouselWrapper.style.transform = `translateX(-${currentSlide * slideWidth}px)`;
 }
 
 function reloadAllIframes() {
-    const iframes = document.querySelectorAll('iframe'); // Seleciona todos os iframes no DOM
+	const iframes = document.querySelectorAll("iframe"); // Seleciona todos os iframes no DOM
 
-    for (const iframe of iframes) {
-        reloadIframe(iframe); // Chama a função reloadIframe para cada iframe
-    }
+	for (const iframe of iframes) {
+		reloadIframe(iframe); // Chama a função reloadIframe para cada iframe
+	}
 }
 
 function prevSlide() {
-    const totalSlides = document.querySelectorAll('.carousel-item').length;
-    currentSlide = (currentSlide > 0) ? currentSlide - 1 : totalSlides - 1;
-    updateCarousel();
+	const totalSlides = document.querySelectorAll(".carousel-item").length;
+	currentSlide = currentSlide > 0 ? currentSlide - 1 : totalSlides - 1;
+	updateCarousel();
 
-    // Chamada da função `reloadIframe` a cada mudança de slide
-    reloadIframe(document.querySelectorAll('.carousel-item')[currentSlide]);
-    reloadAllIframes();
+	// Chamada da função `reloadIframe` a cada mudança de slide
+	reloadIframe(document.querySelectorAll(".carousel-item")[currentSlide]);
+	reloadAllIframes();
 }
 
 function nextSlide() {
-    const totalSlides = document.querySelectorAll('.carousel-item').length;
-    currentSlide = (currentSlide < totalSlides - 1) ? currentSlide + 1 : 0;
-    updateCarousel();
+	const totalSlides = document.querySelectorAll(".carousel-item").length;
+	currentSlide = currentSlide < totalSlides - 1 ? currentSlide + 1 : 0;
+	updateCarousel();
 
-    // Chamada da função `reloadIframe` a cada mudança de slide
-    reloadIframe(document.querySelectorAll('.carousel-item')[currentSlide]);
-    reloadAllIframes();
+	// Chamada da função `reloadIframe` a cada mudança de slide
+	reloadIframe(document.querySelectorAll(".carousel-item")[currentSlide]);
+	reloadAllIframes();
 }
 
-
-window.addEventListener('resize', updateCarousel);
+window.addEventListener("resize", updateCarousel);
